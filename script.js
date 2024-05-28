0c7962a06ae40dd9dc4883409b85a15c59d04eb1
federico cubesino <fede_cubesino@hotmail.com>
2024-05-28 17:21:12 -0300

    Agrego funcionalidades como: chequeo de numero de tel valido, generacion de mensajes de error y estilos.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
     let mesActual;
     let fechaSeleccionada;
     let horarioSeleccionado;
+    let reservaRealizada;
     const horariosReservadosPorFecha = {};  
      
         /* Obtener los datos guardados del almacenamiento local al cargar la página */
@@ -62,6 +63,8 @@
      /* Evento para cambiar al mes anterior */
 
     document.getElementById("mesAnterior").addEventListener("click", function() {
+        contenedorHorarios.style.display = "none";
+        formularioReserva.style.display = "none";
         mesActual--;
         if (mesActual < 0) {
             mesActual = 11;
@@ -73,6 +76,8 @@
     /*  Evento para cambiar al mes siguiente */
 
     document.getElementById("mesSiguiente").addEventListener("click", function() {
+        contenedorHorarios.style.display = "none";
+        formularioReserva.style.display = "none";
         mesActual++;
         if (mesActual > 11) {
             mesActual = 0;
@@ -84,6 +89,8 @@
      /* Evento para seleccionar una fecha específica */
 
     calendario.querySelector(".dias").addEventListener("click", function(event) {
+        contenedorHorarios.style.display = "none";
+        formularioReserva.style.display = "none";
         const dia = event.target.textContent;
         fechaSeleccionada = new Date(anoActual, mesActual, dia);
         mostrarHorariosDisponibles(fechaSeleccionada);
@@ -99,39 +106,52 @@
         /* Horarios disponibles para el día seleccionado */
         const horariosDisponibles = ["09:00hs", "10:00hs", "11:00hs", "12:00hs", "13:00hs", "14:00hs", "15:00hs"];
         const eligeHorario = document.createElement("div");
-        eligeHorario.textContent = "Elige un horario para:" + fecha.toLocaleDateString('es-ES');
+        eligeHorario.textContent = "Elige un horario para el  " + fecha.toLocaleDateString('es-ES');
         eligeHorario.style.fontWeight = "bold";
         contenedorHorarios.appendChild(eligeHorario);
     
         /* Mostrar las opciones de horarios disponibles */
-        horariosDisponibles.forEach(function(horario) {
-            const opcion = document.createElement("div");
-            opcion.classList.add("horario");
-            opcion.textContent = horario;
-            opcion.style.lineHeight = "25px";
+        const contenedorHorariosDiv = document.createElement("div");
+contenedorHorariosDiv.classList.add("horarios-container");
+        // Supongamos que 'horariosDisponibles' es tu array de horarios
+horariosDisponibles.forEach(function(horario) {
+    // Crear un nuevo div para cada horario
+    const opcion = document.createElement("div");
+    opcion.classList.add("horario");
+    
+    // Establecer el texto del div de la opción como el horario
+    opcion.textContent = horario;
+
+    // Establecer el estilo del div de la opción
+    opcion.style.lineHeight = "5px";
             
             // Verificar si el horario está reservado
             if (horariosReservadosParaFecha.some(reserva => reserva.horario === horario)) {
                 opcion.classList.add("reservado");
             }
     
-            contenedorHorarios.appendChild(opcion);
+            contenedorHorariosDiv.appendChild(opcion);
         });
+        contenedorHorarios.appendChild(contenedorHorariosDiv);
     };
 
      /* Evento para seleccionar un horario */
 
      contenedorHorarios.addEventListener("click", function(event) {
+
         if (event.target.classList.contains("horario")) {
             horarioSeleccionado = event.target.textContent;
             const fechaKey = fechaSeleccionada.toLocaleDateString('es-ES');
             const horariosReservados = horariosReservadosPorFecha[fechaKey];
             if (horariosReservados && horariosReservados.some(reserva => reserva.horario === horarioSeleccionado)) {
-                
                 const reserv = document.createElement("div");
                 reserv.classList.add("reserv");
-                reserv.textContent = "\nEl horario no esta disponible";
+                reserv.textContent = "Los horarios en rojo no estan disponibles, elije otro por favor.";
+                reserv.style.color = "red"
                 contenedorHorarios.appendChild(reserv);
+                contenedorHorarios.addEventListener("click", function(event) {
+                    reserv.style.display = "none";
+                });
                 
             } else {
                 mostrarFormularioReserva();
@@ -139,13 +159,6 @@
             }
         }
     });
-    
-    
-    
-    
-
-    
-    
     
      /* Mostrar el formulario de reserva */
     function mostrarFormularioReserva() {
@@ -155,12 +168,36 @@
      /* Maneja el envío del formulario */
     formularioReserva.addEventListener("submit", function(event) {
         event.preventDefault();
-        const nombre = document.getElementById("nombre").value;
-        const telefono = document.getElementById("telefono").value;
+        const nombre = document.getElementById("nombre").value.toUpperCase();
+        const telefonoInput = document.getElementById("telefono");
+        const telefono = telefonoInput.value;
 
+        const regexTelefonoArgentino = /^\(?(0)?(11|[2368]\d)([ \-])?(\d{4})[ \-]?(\d{4})$/;
+
+        // Verificar si el número de teléfono coincide con el formato esperado
+        telefonoInput.value = "";
+        
+        if (!regexTelefonoArgentino.test(telefono)) {
+            // Crear un elemento de mensaje de error
+            
+            const mensajeError = document.createElement("div");
+            mensajeError.style.display = "flex";
+            mensajeError.textContent = "El número de teléfono ingresado no es válido. Por favor, ingrese un número válido.";
+            mensajeError.style.color = "red";
+            mensajeError.classList.add("mensaje-error")
+            telefonoInput.addEventListener("click", function(event) {
+                mensajeError.style.display = "none";
+            });
+            
+    
+            // Agregar el mensaje de error al contenedor
+            formularioReserva.appendChild(mensajeError);
+    
+            return; // Detener el proceso de envío del formulario si el número no es válido
+        }
+        
          /* Guardar el horario reservado para la fecha seleccionada */
-        
-        
+
          if (!horariosReservadosPorFecha[fechaSeleccionada.toLocaleDateString('es-ES')]) {
             horariosReservadosPorFecha[fechaSeleccionada.toLocaleDateString('es-ES')] = [];
         }
@@ -170,30 +207,27 @@
             telefono: telefono
         });
 
-        
-        
-
-    var mensaje = "Fecha\t\tHorarios reservados\t\tNombre\n";
-    mensaje += "--------------------------------------------\n";
-
-    // Iterar sobre las claves y valores del objeto horariosReservadosPorFecha
-    Object.keys(horariosReservadosPorFecha).forEach(function(key) {
-        var horarios = horariosReservadosPorFecha[key];
-        mensaje += key + "\t\t";
-        mensaje += horarios.join(", ") + "\t\t" + nombre + "\n";
-    });
-
-
     // Guardar los datos actualizados en el almacenamiento local
     localStorage.setItem("datosReservas", JSON.stringify(horariosReservadosPorFecha));
 
-    alert("¡Reserva realizada con éxito! Nombre: " + nombre + ", Teléfono: " + telefono + ", Fecha: " + fechaSeleccionada.toLocaleDateString('es-ES') + ", Horario: " + horarioSeleccionado);
+    let reservaRealizada = document.createElement("div");
+    reservaRealizada.classList.add("reserva-realizada");
+    reservaRealizada.textContent = "¡Reserva realizada con éxito! Nombre: " + nombre + ", Teléfono: " + telefono + ", Fecha: " + fechaSeleccionada.toLocaleDateString('es-ES') + ", Horario: " + horarioSeleccionado;
+    let botonContinuar = document.createElement("button");
+    botonContinuar.classList.add("btn-cont");
+    botonContinuar.textContent = ("Continuar reservando");
+    calendario.appendChild(reservaRealizada);
+    calendario.appendChild(botonContinuar);
+    botonContinuar.addEventListener("click", function (event) {
+        reservaRealizada.style.display = "none";
+        botonContinuar.style.display = "none";
+    });
+
 
     /* Limpiar el formulario y ocultarlo después de enviar */
     formularioReserva.reset();
     formularioReserva.style.display = "none";
-    contenedorHorarios.style.display = "none";
-    // Construir el mensaje con filas
+    contenedorHorarios.style.display = "noe";
 
         });
 });
